13f0b99a362ca87323acc9abaaa837d15bcec1ca
LErickMG <lemg040798@ciencias.unam.mx>
2020-12-02 11:58:06 -0600

    Header incompleto

--- seminargeotop-a/src/App.js
+++ seminargeotop-a/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header.js";
+import HeaderLinks from "./components/Header/HeaderLinks.js";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header
+        color="transparent"
+        //routes={dashboardRoutes}
+        brand="Material Kit React"
+        rightLinks={<HeaderLinks />}
+        fixed
+        changeColorOnScroll={{
+          height: 400,
+          color: "white"
+        }}
+        {...rest}
+      />
     </div>
   );
 }
