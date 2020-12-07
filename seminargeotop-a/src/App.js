f69ff8775dd9914e3468ea03ad9fb9cb99c2faf4
miguelruor <miguelruiz98@outlook.com>
2020-12-06 19:13:32 -0600

    Boton de Youtube

--- seminargeotop-a/src/App.js
+++ seminargeotop-a/src/App.js
@@ -73,7 +73,7 @@ export default function LandingPage(props) {
         }}
         //{...rest}
       />
-      <Parallax filter image={require("./assets/img/landing-bg.jpg")}>
+      <Parallax filter image={require("./assets/img/images/img1.jpg")}>
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={12} md={6}>
@@ -85,12 +85,12 @@ export default function LandingPage(props) {
               <Button
                 color="danger"
                 size="lg"
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
+                href="https://www.youtube.com/watch?v=lpgcG4ZdmNc&feature=emb_logo"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <i className="fas fa-play" />
-                Watch video
+                Watch our last seminar!
               </Button>
             </GridItem>
           </GridContainer>
