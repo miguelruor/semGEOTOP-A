5072ba8e8ded73b351527aa46313228c13dcf9a2
LErickMG <lemg040798@ciencias.unam.mx>
2020-12-12 17:00:18 -0600

    curr

--- seminargeotop-a/src/views/Components/Sections/SectionCarousel.js
+++ seminargeotop-a/src/views/Components/Sections/SectionCarousel.js
@@ -6,8 +6,6 @@ import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
 import LocationOn from "@material-ui/icons/LocationOn";
 // core components
-import GridContainer from "../../../components/Grid/GridContainer.js";
-import GridItem from "../../../components/Grid/GridItem.js";
 import Card from "../../../components/Card/Card.js";
 
 
@@ -30,56 +28,40 @@ export default function SectionCarousel() {
     autoplay: false
   };
   return (
-    <div className={classes.section} >
-      <div className={classes.container}>
-        <GridContainer style={localStyle.prueba}>
-          <GridItem xs={12} sm={12} md={8} className={classes.marginAuto} >
-            <Card carousel>
-              <Carousel {...settings}>
-                <div>
-                  <img src={image1} alt="First slide" className="slick-image" />
-                  <div className="slick-caption">
-                    <h4>
-                      <LocationOn className="slick-icons" />
-                      Yellowstone National Park, United States
-                    </h4>
-                  </div>
-                </div>
-                <div>
-                  <img
-                    src={image2}
-                    alt="Second slide"
-                    className="slick-image"
-                  />
-                  <div className="slick-caption">
-                    <h4>
-                      <LocationOn className="slick-icons" />
-                      Somewhere Beyond, United States
-                    </h4>
-                  </div>
-                </div>
-                <div>
-                  <img src={image3} alt="Third slide" className="slick-image" />
-                  <div className="slick-caption">
-                    <h4>
-                      <LocationOn className="slick-icons" />
-                      Yellowstone National Park, United States
-                    </h4>
-                  </div>
-                </div>
-              </Carousel>
-            </Card>
-          </GridItem>
-        </GridContainer>
-      </div>
-    </div>
+    <Card carousel>
+      <Carousel {...settings}>
+        <div>
+          <img src={image1} alt="First slide" className="slick-image" />
+          <div className="slick-caption">
+            <h4>
+              <LocationOn className="slick-icons" />
+              Yellowstone National Park, United States
+            </h4>
+          </div>
+        </div>
+        <div>
+          <img
+            src={image2}
+            alt="Second slide"
+            className="slick-image"
+          />
+          <div className="slick-caption">
+            <h4>
+              <LocationOn className="slick-icons" />
+              Somewhere Beyond, United States
+            </h4>
+          </div>
+        </div>
+        <div>
+          <img src={image3} alt="Third slide" className="slick-image" />
+          <div className="slick-caption">
+            <h4>
+              <LocationOn className="slick-icons" />
+              Yellowstone National Park, United States
+            </h4>
+          </div>
+        </div>
+      </Carousel>
+    </Card>
   );
 }
-
-const localStyle={
-  prueba: {
-    backgroundColor: 'black',
-    justifyContent: 'center',
-    alignItems: 'center',  
-  },
-};
