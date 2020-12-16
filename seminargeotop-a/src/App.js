7a9c3bf702f68892371cdadde17926cbe4e2f7a8
miguelruor <miguelruiz98@outlook.com>
2020-12-16 01:48:51 -0600

    New menu option

--- seminargeotop-a/src/App.js
+++ seminargeotop-a/src/App.js
@@ -8,6 +8,8 @@ import HomePage from './views/HomePage/HomePage';
 import NextTalksPage from './views/NextTalksPage/NextTalksPage';
 import PreviousTalksPage from './views/PreviousTalksPage/PreviousTalksPage';
 import SubscribePage from './views/SubscribePage/SubscribePage';
+import ListKeywordsPage from './views/ListKeywordsPage/ListKeywordsPage';
+import ListSpeakersPage from './views/ListSpeakersPage/ListSpeakersPage';
 
 var hist = createBrowserHistory();
 
@@ -18,6 +20,8 @@ export default function App(props) {
         <Route path="/NextTalks" component={NextTalksPage} />
         <Route path="/PreviousTalks" component={PreviousTalksPage} />
         <Route path="/Subscribe" component={SubscribePage} />
+        <Route path="/ListKeywords" component={ListKeywordsPage} />
+        <Route path="/ListSpeakers" component={ListSpeakersPage} />
         <Route path="/" component={HomePage} />
       </Switch>
     </Router>
