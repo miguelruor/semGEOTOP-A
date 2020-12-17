840ed1df20b4c15191bfb325e7b00b5484d448c5
Randy Ibarra <randyosbaldo@gmail.com>
2020-12-16 22:39:20 -0600

    lee imagenes de carpetas e imprime en carousel

--- seminargeotop-a/src/views/Components/Sections/SectionCarousel.js
+++ seminargeotop-a/src/views/Components/Sections/SectionCarousel.js
@@ -8,31 +8,36 @@ import LocationOn from "@material-ui/icons/LocationOn";
 // core components
 import Card from "../../../components/Card/Card.js";
 
-
-import image1 from "../../../assets/img/bg.jpg";
-import image2 from "../../../assets/img/bg2.jpg";
-import image3 from "../../../assets/img/bg3.jpg";
-
 import styles from "../../../assets/scss/plugins/_plugin-react-slick.scss";
 
 const useStyles = makeStyles(styles);
 
-function CarouselImage(props) {
+function renderCarouselImage(src) {
   return (
     <div>
       <img 
-        src={props.src} 
+        src={src} 
         alt="First slide" 
         className="slick-image" />
     </div>
-    <button className="square" onClick={props.src}>
-      {props.value}
-    </button>
   );
 }
 
+function renderImages(images) {
+  const n_images = images.length
+
+  let imgs = []
+  for (let i = 0; i<n_images; i++) {
+    imgs.push(renderCarouselImage(images[i].default));
+  }
+
+  return imgs;
+}
+
 export default function SectionCarousel() {
+  
   const classes = useStyles();
+  
   const settings = {
     dots: true,
     infinite: true,
@@ -41,36 +46,19 @@ export default function SectionCarousel() {
     slidesToScroll: 1,
     autoplay: true
   };
+
+  // Funcion para leer todos los archivos de una carpeta (npm install --save-dev webpack@4.44.2 webpack-cli)
   function importAll(r) {
     return r.keys().map(r);
   }
-  const images = importAll(require.context('../../../assets/img/images/Pics_Geometry', true, /\.(png|jpe?g|svg)$/));
-  console.log(typeof(images));
-  console.log(images.length);
-  console.log(images[0].default);
-  console.log(image1);
+  const geometryImages = importAll(require.context('../../../assets/img/images/Pics_Geometry', true, /\.(png|jpe?g|svg)$/));
+  const topologyImages = importAll(require.context('../../../assets/img/images/Pics_Topology', true, /\.(png|jpe?g|svg)$/));
+  
   return (
     <Card carousel>
       <Carousel {...settings}>
-        <div>
-          <img 
-            src={images[0].default} 
-            alt="First slide" 
-            className="slick-image" />
-        </div>
-        <div>
-          <img
-            src={images[1].default}
-            alt="Second slide"
-            className="slick-image"
-          />
-        </div>
-        <div>
-          <img 
-            src={images[2].default} 
-            alt="Third slide" 
-            className="slick-image" />
-        </div>
+        {renderImages(geometryImages)}
+        {renderImages(topologyImages)}
       </Carousel>
     </Card>
   );
