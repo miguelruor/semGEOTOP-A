1443391de473981b0320c1e07d2316cea26ce0e4
miguelruor <miguelruiz98@outlook.com>
2020-12-17 14:59:12 -0600

    Datos para previous talks

--- seminargeotop-a/src/views/PreviousTalksPage/Sections/LeftMenuSection.js
+++ seminargeotop-a/src/views/PreviousTalksPage/Sections/LeftMenuSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -9,10 +9,80 @@ import NavPills from "../../../components/NavPills/NavPills.js";
 
 import styles from "../../../assets/jss/material-kit-react/views/landingPageSections/teamStyle.js";
 
+import {db} from '../../../ConfigFirebase';
+
 const useStyles = makeStyles(styles);
 
 export default function LeftMenuSection(){
     const classes = useStyles();
+
+    var month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+
+    var speakers_aux = {};
+    var seasons_aux = {};
+
+    const [seasons, setSeasons] = useState({});
+
+    useEffect(async()=>{
+        await db.collection("speakers").get()
+        .then(function(querySnapshot){
+            querySnapshot.forEach(function(doc){
+                var idSpeaker = doc.id;
+                var mi = doc.data().middle_initial;
+
+                speakers_aux[idSpeaker] = doc.data().name + " " + 
+                    (mi != null ? mi : "") + " " + doc.data().surname;
+            });
+            //alert("Carga de speakers exitosa")
+        })
+        .catch(function(error){
+            alert("Cannot load some speaker");
+        });
+
+        await db.collection("talks").orderBy("date", "desc")
+        .get()
+        .then(function(querySnapshot){
+            querySnapshot.forEach(function(doc){
+                var sea = doc.data().season;
+                if(!(sea in seasons_aux)){
+                    seasons_aux[sea] = []
+                }
+                
+                const speakerID = doc.data().speaker;
+                const date = doc.data().date.toDate();
+
+                seasons_aux[sea].push(
+                    {
+                        speaker: speakers_aux[speakerID],
+                        title: doc.data().title,
+                        keywords: doc.data().keywords,
+                        date: month[date.getMonth()] + date.getDay().toString()+", " + date.getFullYear().toString(),
+                        abstract: doc.data().abstract,
+                        video: doc.data().video,
+                    }
+                );
+                //alert(speakerID)
+            });
+        })
+        .catch(function(error){
+            alert(error);
+        });
+
+        setSeasons(seasons_aux);
+    },[]);
+
     return(
         <NavPills
             color="transparent"
