2ca3f018c60e830a1dd79f655a7d161ae5c09f72
miguelruor <miguelruiz98@outlook.com>
2020-12-17 16:47:38 -0600

    speakers img

--- seminargeotop-a/src/views/NextTalksPage/Sections/NextTalksSection.js
+++ seminargeotop-a/src/views/NextTalksPage/Sections/NextTalksSection.js
@@ -15,7 +15,7 @@ import CardBody from "../../../components/Card/CardBody.js";
 import CardFooter from "../../../components/Card/CardFooter.js";
 
 import styles from "../../../assets/jss/material-kit-react/views/landingPageSections/teamStyle.js";
-import image1 from "../../../assets/img/images/sp41.jpg";
+import image1 from "../../../assets/img/images/speakers/sp41.jpg";
 
 const useStyles = makeStyles(styles);
 
